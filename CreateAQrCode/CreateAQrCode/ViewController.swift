97f1d5cd97d0bf3d284a67e8a4cd91ce1ca8bf5c
cagdaseksi <cagdas.eksi@hotmail.com>
2019-07-19 09:59:50 +0300

    How to create a QR code

---
+++ CreateAQrCode/CreateAQrCode/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CreateAQrCode
+//
+//  Created by MAC on 19.07.2019.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = generateQRCode(from: "Let's learn swift.")
+        
+        img.image = image
+        
+    }
+
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        
+        return UIImage()
+    }
+    
+}
+
