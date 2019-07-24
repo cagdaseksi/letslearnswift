b02c3eccd665f3a95d15e8a39c2460710c66dada
cagdaseksi <cagdas.eksi@hotmail.com>
2019-07-24 11:53:25 +0300

    load a remote image URL
    How to load a remote image URL into UIImageView

---
+++ LoadRemoteImageUrl/LoadRemoteImageUrl/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  LoadRemoteImageUrl
+//
+//  Created by MAC on 24.07.2019.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80")
+        
+        image.load(url: url!)
+        
+    }
+
+
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
