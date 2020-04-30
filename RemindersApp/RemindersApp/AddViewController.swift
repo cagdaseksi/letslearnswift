cd208643aeb852a1482af0edc030033cf3366686
cagdaseksi <cagdas.eksi@hotmail.com>
2020-04-30 16:09:50 +0300

    Reminder App

---
+++ RemindersApp/RemindersApp/AddViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddViewController.swift
+//  RemindersApp
+//
+//  Created by MAC on 30.04.2020.
+//  Copyright © 2020 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var bodyField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+
+    public var completion: ((String, String, Date) -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleField.delegate = self
+        bodyField.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
+    }
+
+    @objc func didTapSaveButton() {
+        
+        if let titleText = titleField.text, !titleText.isEmpty,
+            let bodyText = bodyField.text, !bodyText.isEmpty {
+
+            let targetDate = datePicker.date
+
+            completion?(titleText, bodyText, targetDate)
+
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+}
---
+++ RemindersApp/RemindersApp/ViewController.swift
@@ -0,0 +1,167 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by MAC on 30.04.2020.
+//  Copyright © 2020 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+
+    var models = [Reminder]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnReflesh(_ sender: Any) {
+    }
+    
+    @IBAction func btnAdd() {
+        print("acşkadkj")
+        guard let vc = storyboard?.instantiateViewController(identifier: "AddViewController") as? AddViewController else {
+            print("guard")
+            return
+        }
+        
+        vc.title = "New Reminder"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        
+        vc.completion = { title, body, date in
+            DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                
+                let new = Reminder(title: title,body: body, date: date, identifier: "id_\(title)")
+                
+                self.models.append(new)
+                
+                self.tableView.reloadData()
+
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.sound = .default
+                content.body = body
+
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate),repeats: false)
+
+                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let row = models[indexPath.row]
+        
+        cell.textLabel?.text = row.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd, MMMM, YYYY"
+        cell.detailTextLabel?.text = formatter.string(from: row.date)
+
+        cell.textLabel?.font = UIFont(name: "Arial", size: 25)
+        cell.detailTextLabel?.font = UIFont(name: "Arial", size: 22)
+        return cell
+    }
+
+}
+
+extension ViewController {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 45
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: nil) {(action, sourceView, completionHandler) in
+            
+            self.models.remove(at: indexPath.row)
+            self.tableView.reloadData()
+            
+            completionHandler(true)
+        }
+        
+        deleteAction.image = UIImage(systemName: "trash")
+        deleteAction.backgroundColor = .red
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let doneAction = UIContextualAction(style: .normal, title: nil) {(action, sourceView, completionHandler) in
+            
+            let row = self.models[indexPath.row]
+            
+            self.scheduleTest(title: row.title, body: row.body)
+            
+            completionHandler(true)
+        }
+        
+        doneAction.image = UIImage(systemName: "checkmark")
+        doneAction.backgroundColor = #colorLiteral(red: 0.231372549, green: 0.7411764706, blue: 0.6509803922, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [doneAction])
+    }
+    
+    func scheduleTest(title: String, body: String) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.sound = .default
+        content.body = body
+
+        let targetDate = Date().addingTimeInterval(10)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
+                                                                                                  from: targetDate),
+                                                    repeats: false)
+
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print("something went wrong")
+            }
+        })
+    }
+    
+}
+
+struct Reminder {
+    let title: String
+    let body: String
+    let date: Date
+    let identifier: String
+}
